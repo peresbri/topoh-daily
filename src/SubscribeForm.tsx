f8af78365039fd6648a96593d53560fa253e4a65
B M M <bmm@Bs-MacBook-Pro.local>
2020-08-07 22:34:35 -0400

    add makestate function

--- src/SubscribeForm.tsx
+++ src/SubscribeForm.tsx
@@ -3,18 +3,39 @@ import { Input, Button } from "antd";
 
 interface IProps {}
 
-interface IState {}
+interface IState {
+  name: string;
+  email: string;
+}
+
+function makeState(state?: IState): IState {
+  if (!!state) {
+    return {...state};
+  }
+  
+  return {
+    name: 'Joe Doe',
+    email: 'tina@topoh.cm'
+  }
+}
 
 export class SubscribeForm extends React.Component<IProps, IState> {
+
+  constructor(public props: IProps, public state: IState) {
+    super(props);
+
+    this.state = makeState();
+  }
+
   render() {
     return (
       <div>
         <p>
           Enter your email to get daily <span>topoh</span>
         </p>
-        <Input size="large" value="Joe Doe" placeholder="Joe Doe" />
-        <Input size="large" value="tina@topoh.cm" placeholder="tina@topoh.cm" />
-        <Button type="primary">Subscribe</Button>
+        <Input size="large" value={this.state.name} placeholder="Joe Doe" />
+        <Input size="large" value={this.state.email} placeholder="tina@topoh.cm" />
+        <Button size="large" block={true} type="primary">Subscribe</Button>
       </div>
     );
   }
